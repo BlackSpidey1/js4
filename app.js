c301b93482afe85b951fe91031676648f5cbf3f9
durdona <123@.ru>
2023-09-28 22:46:48 +0500

    final

---
+++ app.js
@@ -0,0 +1,62 @@
+/* /* Задание 1 */
+
+// let userName = prompt("Bведите имя");
+// let currentYear = prompt("Введите нынешний год");
+// let birth = prompt("Введите год рождения");
+// function ageCals(currentYear, birth) {
+//   let userAge = currentYear - birth;
+//   return ` ${userName}, Ваш возраст ${userAge} `;
+// }
+
+// console.log(ageCals(currentYear, birth)); */
+
+
+
+
+/* // Задание 2
+
+// function rand(min,max){
+//     return Math.floor(Math.random() * (max + 1 -min ) +min)
+    
+// }
+
+// function operationRand(){
+//     return Math.floor(Math.random() * (5-1) +1)
+// }
+
+
+// let quantity = +prompt('Введите кол-во примеров')
+// let minNumber = +prompt('Введите мин диапазон')
+// let maxNumber = +prompt('Введите макс диапазон')
+
+// for(let i = 1; i <= quantity; i++){
+//     let first = rand(minNumber,maxNumber)
+//     let second = rand(minNumber,maxNumber)
+
+//     let randsym= operationRand()
+//     let aw = 0
+//     if(randsym == 1){
+//     aw=first + second
+//     randsym ='+'
+//     }else if(randsym == 2){
+//     aw=first - second
+//     randsym ='-'
+//     }else if (randsym == 3){
+//     aw=first * second
+//     randsym ='*'
+//     }else if (randsym == 4){
+//     aw=first / second
+//     randsym ='/' 
+//     }
+    
+    
+    
+    
+//     let primer = +prompt(first + randsym + second )
+//     console.log(aw == primer ? `Ваш ответ верный ${primer}`: `Ваш ответ неверный ${primer}||  Правильный ответ ${aw}` );
+    
+
+
+
+//  } */
+
